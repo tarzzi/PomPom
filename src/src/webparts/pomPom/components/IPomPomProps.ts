e5bd9905b26502777ccd6fa6dd983f6ac5cb8a0a
Tarmo Urrio <5585433+tarzzi@users.noreply.github.com>
2023-06-15 20:45:01 +0300

    first commit

---
+++ src/src/webparts/pomPom/components/IPomPomProps.ts
@@ -0,0 +1,7 @@
+export interface IPomPomProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/src/webparts/pomPom/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IPomPomWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'PomPomWebPartStrings' {
+  const strings: IPomPomWebPartStrings;
+  export = strings;
+}
